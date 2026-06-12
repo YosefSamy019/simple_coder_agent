b6b991e09fba009cd9d357be6c878534ad7fe5d4
Youssef Samy <yosefsamy019@gmail.com>
2026-06-13 00:09:32 +0300

    edits

--- models.py
+++ models.py
@@ -39,7 +39,8 @@ class AssistantChatMsg(ChatMsg):
         ]
 
     def to_json(self) -> dict:
-        return {"role": 'assistant', "content": self.content, "tool_calls": self.tool_calls}
+        #"tool_calls": self.tool_calls
+        return {"role": 'assistant', "content": self.content, }
 
 
 class SystemChatMsg(ChatMsg):
