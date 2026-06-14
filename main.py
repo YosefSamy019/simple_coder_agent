5dd58366dd487d1a9e33e907621c14ebaebcbcbf
Youssef Samy <yosefsamy019@gmail.com>
2026-06-14 17:51:28 +0300

    edits

--- main.py
+++ main.py
@@ -305,15 +305,6 @@ def call_model():
 # =====================
 
 def main():
-    CreateFileTool().execute({
-        'filename': 'ali.txt',
-        'content': 'abs'
-    })
-    CreateFileTool().execute({
-        'filename': 'a/moh.txt',
-        'content': 'abs'
-    })
-
     init()
     build_sidebar()
     build_chat()
