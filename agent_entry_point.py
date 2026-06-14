18657ed3d878b4e9964bede9206ee38529292862
Youssef Samy <yosefsamy019@gmail.com>
2026-06-14 18:32:57 +0300

    refactor

--- agent_entry_point.py
+++ agent_entry_point.py
@@ -15,10 +15,10 @@ def main():
 
     if is_goal_achieved():
         st.session_state[AGENT_STATUS] = AgentStatus.STOPPED
+        st.rerun()
 
     if st.session_state.get(AGENT_STATUS) == AgentStatus.RUNNING:
         call_model()
-        st.rerun()
 
 
 if __name__ == "__main__":
