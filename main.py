9999b3bdcb094df60a10673e5d448b03e04e5c49
Youssef Samy <yosefsamy019@gmail.com>
2026-06-14 17:41:20 +0300

    edits

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def init():
         layout="wide",
     )
 
-    st.session_state.setdefault(URL_KEY, 'https://ed32-34-142-231-234.ngrok-free.app')
+    st.session_state.setdefault(URL_KEY, 'https://668e-34-169-186-23.ngrok-free.app')
     st.session_state.setdefault(END_POINT_KEY, '/v1')
     st.session_state.setdefault(CALLS_COUNTER, 0)
     st.session_state.setdefault(FILES_SYSTEM, [])
